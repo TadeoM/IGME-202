873c8bff717dbb249886b30f8bcb28b5211a841f
Gilexarr <tmenichel@gmail.com>
2017-09-19 22:35:00 -0400

    Add files via upload

---
+++ Project1/Assets/scripts/ArmySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmySpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    float Gaussian(float mean, float stdDev)
+    {
+        float val1 = Random.Range(0f, 1f);
+        float val2 = Random.Range(0f, 1f);
+        float gaussValue = Mathf.Sqrt(-2.0f * Mathf.Log(val1)) * Mathf.Sin(2.0f * Mathf.PI * val2);
+        return mean + stdDev * gaussValue; }
+}
---
+++ Project1/Assets/scripts/TerrainGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour {
+
+    //
+
+
+	// Use this for initialization
+	void Start () {
+        // 
+
+        // call generate
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+
+    void GenerateTerrain()
+    {
+        // initialize coordinates to 0
+        // go through each row and column of terrain data
+        // in each for loop, use appropriate coord and add it to 1/resolution(or coord?)
+    }
+}
